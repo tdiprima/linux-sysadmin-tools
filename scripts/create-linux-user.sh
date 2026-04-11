99001a21a4226e7dc0777465fa47ef22311b4955
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2026-04-11 09:50:27 -0400

    create user

--- scripts/create-linux-user.sh
+++ scripts/create-linux-user.sh
@@ -7,8 +7,12 @@ echo "Creating user: $USERNAME"
 # Create user with home directory
 sudo useradd -m "$USERNAME"
 
+# 👉 This is the recommended way on Ubuntu:
+# sudo adduser newusername
+
 # Set password (change after first login)
 echo "$USERNAME:password123" | sudo chpasswd
+# sudo passwd newusername
 
 # Add user to sudo group
 sudo usermod -aG sudo "$USERNAME"
